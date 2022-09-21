dfac170d3cc964484c4d0407c3a5b71a43cdc5fd
Eldad Danladi <eldaddanladi@gmail.com>
2022-09-21 11:16:34 -0600

    feat(framework): framework switch feat(page): Login page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      {/* Hellow World */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
